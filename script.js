2cfd9059b33d3697279bd16bea9122cf6fa8ece2
henkiehenkiehenkie <maxwellmichaspam@gmail.com>
2024-08-13 17:29:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const chooseButton = document.getElementById('chooseButton');
+    
+    chooseButton.addEventListener('click', function() {
+        alert('Je hebt op de knop geklikt!');
+    });
+});
