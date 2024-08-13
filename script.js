cad87a28b7cfa459eb7c2851d81ad7bb3905a5b5
henkiehenkiehenkie <maxwellmichaspam@gmail.com>
2024-08-13 17:46:44 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const chooseButton = document.getElementById('chooseButton');
-    chooseButton.addEventListener('click', function() {
-        // Redirect naar google.nl
-        window.location.href = 'https://www.google.nl';
-    });
-});
