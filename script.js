e80b1157c6807b23232d9aba54ec5ac30da453ab
henkiehenkiehenkie <maxwellmichaspam@gmail.com>
2024-08-13 17:36:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function() {
     const chooseButton = document.getElementById('chooseButton');
-    
     chooseButton.addEventListener('click', function() {
         // Redirect naar google.nl
         window.location.href = 'https://www.google.nl';
