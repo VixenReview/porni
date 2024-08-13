4b9450b79b21c216f1b7154c6b14c2783b9621d6
henkiehenkiehenkie <maxwellmichaspam@gmail.com>
2024-08-13 17:47:01 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const chooseButton = document.getElementById('chooseButton');
+    
+    chooseButton.addEventListener('click', function() {
+        // Redirect naar google.nl
+        window.location.href = 'https://www.google.nl';
+    });
+});
